fc9971abcdf209b2ad17880ca30c6a06ab108ded
jescomarketing <jesco.marketing@gmail.com>
2016-01-26 20:00:55 -0600

    Agregando calidaciones

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ActiveRecord::Base
+	validates :control, :nombre, :appelidos, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { :with => VALID_EMAIL_REGEX , message: "El formato del correo es invalido" }
+	validates :telefono, :celular, numericality: { only_integer: true }
 end
